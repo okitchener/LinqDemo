572c74f6485fecfc333ba2a38ff1ec340d269e5e
Owen Kitchener <okitchener@my.wctc.edu>
2025-10-21 12:49:58 -0500

    [1.04] Create list of characters from json data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+// deserialize mario json from file into List<Mario>
+List<Character> dks = JsonSerializer.Deserialize<List<Character>>(File.ReadAllText("dk.json"))!;
+// deserialize mario json from file into List<Mario>
+List<Character> marios = JsonSerializer.Deserialize<List<Character>>(File.ReadAllText("mario.json"))!;
+// combine all characters into 1 list
+List<Character> characters = [];
+characters.AddRange(dks);
+characters.AddRange(marios);
+
+// display all characters
+foreach(Character character in characters)
+{
+  Console.WriteLine(character.Display());
+}
