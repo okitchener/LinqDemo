a8347a4cb38ef209a03e89dd7926a578c64ccf0f
Owen Kitchener <okitchener@my.wctc.edu>
2025-10-21 12:59:59 -0500

    [1.09] Display last character

--- Program.cs
+++ Program.cs
@@ -32,7 +32,10 @@ Console.Clear();
 // }
 
 //display characters 6-10
-foreach(Character character in characters.Skip(5).Take(5))
-{
-    Console.WriteLine(character.Display());
-}
+// foreach(Character character in characters.Skip(5).Take(5))
+// {
+//     Console.WriteLine(character.Display());
+// }
+
+//display last character
+Console.WriteLine(characters.Last().Display());
