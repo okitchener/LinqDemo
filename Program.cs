7a0d6098b75ba8798beab7c93bca7546b873d703
Owen Kitchener <okitchener@my.wctc.edu>
2025-10-21 13:11:31 -0500

    [1.14d] Display which characters were created in 1995 (name & description only)

--- Program.cs
+++ Program.cs
@@ -52,8 +52,8 @@ bool Character1995 = characters.Any(c => c.YearCreated == 1995);
 Console.WriteLine($"Are there any characters created in 1995: {Character1995}");
 // how many characters were created in 1995
 Console.WriteLine($"How many? {characters.Count(c => c.YearCreated == 1995)}");
-//which characters were created in 1995 (name only)
-foreach(String? name in characters.Where(c => c.YearCreated == 1995).Select(c => c.Name))
+//which characters were created in 1995 (name & description only)
+foreach(var obj in characters.Where(c => c.YearCreated == 1995).Select(c => new { c.Name, c.Description}))
 {
-  Console.WriteLine(name);
+  Console.WriteLine($"{obj.Name} - {obj.Description}");
 }
