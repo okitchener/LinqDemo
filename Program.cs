9d4e928e599ce8fbf3b7cb31a7cac1cb02b07d81
Owen Kitchener <okitchener@my.wctc.edu>
2025-10-21 12:56:56 -0500

    [1.07] Display every character except the 1st 5

--- Program.cs
+++ Program.cs
@@ -20,7 +20,13 @@ Console.Clear();
 // Console.WriteLine(characters.First().Display());
 
 //display the first 5 characters
-foreach(Character character in characters.Take(5))
+// foreach(Character character in characters.Take(5))
+// {
+//     Console.WriteLine(character.Display());
+// }
+
+//display every character except the first 5
+foreach(Character character in characters.Skip(5))
 {
     Console.WriteLine(character.Display());
 }
