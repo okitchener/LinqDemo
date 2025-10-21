561e8e40d2b3b9d5df92b1186b347643717162d5
Owen Kitchener <okitchener@my.wctc.edu>
2025-10-21 13:24:53 -0500

    [1.16] Display which character(s) has/have the most alias's

--- Program.cs
+++ Program.cs
@@ -59,22 +59,28 @@ Console.Clear();
 // }
 
 //how many characters in total (all series)?
-int CharacterCount = characters.Count();
-Console.WriteLine($"There are {CharacterCount} characters in all series");
+//int CharacterCount = characters.Count();
+//Console.WriteLine($"There are {CharacterCount} characters in all series");
 // how many characters appear in the Mario series?
-int MarioCount = characters.Where(c => c.Series.Contains("Mario")).Count();
-Console.WriteLine($"There are {MarioCount} characters in the Mario series");
+//int MarioCount = characters.Where(c => c.Series.Contains("Mario")).Count();
+//Console.WriteLine($"There are {MarioCount} characters in the Mario series");
 // how many characters appear in the Donkey Kong series?
-int DkCount = characters.Where(c => c.Series.Contains("Donkey Kong")).Count();
-Console.WriteLine($"There are {DkCount} characters in the Donkey Kong series");
+//int DkCount = characters.Where(c => c.Series.Contains("Donkey Kong")).Count();
+//Console.WriteLine($"There are {DkCount} characters in the Donkey Kong series");
 // how many characters appear in the Mario & Donkey Kong series?
-int DkMarioCount = characters.Where(c => c.Series.Contains("Donkey Kong") && c.Series.Contains("Mario")).Count();
-Console.WriteLine($"There are {DkMarioCount} characters that appear in Mario and Donkey Kong series");
+//int DkMarioCount = characters.Where(c => c.Series.Contains("Donkey Kong") && c.Series.Contains("Mario")).Count();
+//Console.WriteLine($"There are {DkMarioCount} characters that appear in Mario and Donkey Kong series");
 // which characters (name only) appear in Mario and Donkey Kong?
-foreach (String? name in characters.Where(c => c.Series.Contains("Donkey Kong") && c.Series.Contains("Mario")).Select(c => c.Name))
-{
-    Console.WriteLine($"\t{name}");
-}
+//foreach (String? name in characters.Where(c => c.Series.Contains("Donkey Kong") && c.Series.Contains("Mario")).Select(c => c.Name))
+//{
+//    Console.WriteLine($"\t{name}");
+//}
 // how many characters appear in Donkey Kong and not in Mario?
-int DkNotMarioCount = characters.Where(c => c.Series.Contains("Donkey Kong") && !c.Series.Contains("Mario")).Count();
-Console.WriteLine($"There are {DkNotMarioCount} characters that appear in Donkey Kong and Not in Mario series");
+//int DkNotMarioCount = characters.Where(c => c.Series.Contains("Donkey Kong") && !c.Series.Contains("Mario")).Count();
+//Console.WriteLine($"There are {DkNotMarioCount} characters that appear in Donkey Kong and Not in Mario series");
+
+
+// which character(s) has/have the most alias's?
+foreach(var obj in characters.Where(c => c.Alias.Count() == characters.Max(c => c.Alias.Count())).Select(c => new {c.Name, c.Alias})){
+  Console.WriteLine($"{obj.Name} has {obj.Alias.Count()} alias(s):\n\t{String.Join(", ", obj.Alias)}");
+}
